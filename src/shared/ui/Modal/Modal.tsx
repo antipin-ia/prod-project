d6b6e9617e5b96876169ddbfc424f829a9cf2865
vanechka <antipinivan0491@gmail.com>
2024-03-29 14:11:50 +0300

    add json-server

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -4,6 +4,7 @@ import React, {
     ReactNode, useEffect, useRef, useState,
 } from 'react';
 import { Portal } from 'shared/ui/Portal/Portal';
+import { useTheme } from 'app/providers/ThemeProvider';
 import styles from './Modal.module.scss';
 
 interface ModalProps {
@@ -27,6 +28,8 @@ export const Modal = (props: ModalProps) => {
     const [isClosing, setIsClosing] = useState(false);
 
     const timerRef = useRef < ReturnType<typeof setTimeout>>();
+
+    const { theme } = useTheme();
 
     const closeHandler = () => {
         if (onClose) {
@@ -64,7 +67,7 @@ export const Modal = (props: ModalProps) => {
 
     return (
         // <Portal>
-        <div className={classNames(styles.Modal, mods, [className])}>
+        <div className={classNames(styles.Modal, mods, [className, theme])}>
             <div
                 className={styles.overlay}
                 onClick={closeHandler}
